f6de02d006abef4240bb56448e396c6097bcc7ec
Reagan Armstrong <reagancstr@gmail.com>
2026-03-09 17:52:03 -0400

    Removed unused imports in signup page.

--- ui/app/signup/page.tsx
+++ ui/app/signup/page.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { UserPlus } from 'lucide-react';
