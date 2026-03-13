7a71f259efd311da48402fc3f6ae5a55cc93aa1e
Reagan Armstrong <reagancstr@gmail.com>
2026-03-13 10:33:36 -0400

    Added variable file for domain information.

---
+++ infra/modules/certs/variables.tf
@@ -0,0 +1,9 @@
+variable "domain_name" {
+    type = string
+    description = "Root domain name."
+}
+
+variable "cloudflare_zone_id" {
+  type = string
+  description = "Cloudflare domain zone id."
+}
