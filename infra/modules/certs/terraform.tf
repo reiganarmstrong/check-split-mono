f5d03d3b89a190ba3f09ff52fe1cba7a007a6243
Reagan Armstrong <reagancstr@gmail.com>
2026-03-13 10:34:05 -0400

    Moved provider configuration logic into terraform.tf for better separation of concerns.

--- infra/modules/certs/terraform.tf
+++ infra/modules/certs/terraform.tf
@@ -10,3 +10,12 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "default"
+}
+
+provider "cloudflare" {
+  # CLOUDFLARE_API_TOKEN environment variable used for authentication
+}
