79d72b3214cc01a3252ca338ead19d23e05395fb
Reagan Armstrong <reagancstr@gmail.com>
2026-03-16 11:58:18 -0400

    Updated dev environment to use appropriate variables for modules.

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -1,10 +1,18 @@
 module "certs" {
-  source = "../modules/certs"
+  source             = "../../modules/certs"
+  environment        = var.environment
+  root_domain        = var.root_domain
+  subdomain          = var.subdomain
+  cloudflare_zone_id = var.cloudflare_zone_id
 }
 
 module "auth" {
-  source             = "../modules/auth"
+  source             = "../../modules/auth"
   validated_cert_arn = module.certs.validated_cert_arn
+  environment        = var.environment
+  root_domain        = var.root_domain
+  subdomain          = var.subdomain
+  cloudflare_zone_id = var.cloudflare_zone_id
 }
 
 
