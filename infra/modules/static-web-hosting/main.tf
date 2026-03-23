744bdae46d4c2c94c7637f2ffabf5bf611660ce6
Reagan Armstrong <reagancstr@gmail.com>
2026-03-23 16:31:44 -0400

    Added module imports into static web hosting module and appropriate variables.

---
+++ infra/modules/static-web-hosting/main.tf
@@ -0,0 +1,15 @@
+module "s3-website" {
+  source      = "./s3-website"
+  environment = var.environment
+  bucket_name = var.bucket_name
+}
+
+module "cloudfront-cdn" {
+  source                         = "./cloudfront-cdn"
+  s3_bucket_id                   = module.s3-website.s3_bucket_id
+  s3_bucket_arn                  = module.s3-website.s3_bucket_arn
+  s3_bucket_regional_domain_name = module.s3-website.s3_bucket_regional_domain_name
+  cloudfront_custom_domain       = var.cloudfront_custom_domain
+  acm_certificate_arn            = var.acm_certificate_arn
+  cloudflare_zone_id             = var.cloudflare_zone_id
+}
---
+++ infra/modules/static-web-hosting/variables.tf
@@ -0,0 +1,20 @@
+variable "environment" {
+  type = string
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+variable "cloudfront_custom_domain" {
+  type = string
+}
+
+variable "acm_certificate_arn" {
+  type = string
+
+}
+
+variable "cloudflare_zone_id" {
+  type = string
+}
