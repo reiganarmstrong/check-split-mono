31a3123d456243180352f6f265bbf3965af67a5f
Reagan Armstrong <reagancstr@gmail.com>
2026-03-24 14:10:07 -0400

    Fixed type when referencing s3-website outputs.

--- infra/modules/static-website-hosting/main.tf
+++ infra/modules/static-website-hosting/main.tf
@@ -6,9 +6,9 @@ module "s3-website" {
 
 module "cloudfront-cdn" {
   source                         = "./cloudfront-cdn"
-  s3_bucket_id                   = module.s3-website.s3_bucket_id
-  s3_bucket_arn                  = module.s3-website.s3_bucket_arn
-  s3_bucket_regional_domain_name = module.s3-website.s3_bucket_regional_domain_name
+  s3_bucket_id                   = module.s3-website.bucket_id
+  s3_bucket_arn                  = module.s3-website.bucket_arn
+  s3_bucket_regional_domain_name = module.s3-website.bucket_regional_domain
   cloudfront_custom_domain       = var.cloudfront_custom_domain
   acm_certificate_arn            = var.acm_certificate_arn
   cloudflare_zone_id             = var.cloudflare_zone_id
