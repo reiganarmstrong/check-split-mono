73021cde727385c1caa1870fcf0ffd03ab48a6db
Reagan Armstrong <reagancstr@gmail.com>
2026-03-26 14:15:24 -0400

    Created a bootstrap terraform module meant to only be run once to create an OIDC github provider resource in aws.

---
+++ infra/environments/bootstrap/main.tf
@@ -0,0 +1,5 @@
+# run only once so that github can talk to sts
+resource "aws_iam_openid_connect_provider" "github" {
+  url            = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+}
