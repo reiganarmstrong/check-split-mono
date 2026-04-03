6c43f0d4f43c3a75b6295210e88b05f145936664
Reagan Armstrong <reagancstr@gmail.com>
2026-04-03 14:19:27 -0400

    feat: add S3 bucket resources for Terraform state storage with lifecycle management to remove old versions of state files

--- infra/environments/bootstrap/main.tf
+++ infra/environments/bootstrap/main.tf
@@ -3,3 +3,52 @@ resource "aws_iam_openid_connect_provider" "github" {
   url            = "https://token.actions.githubusercontent.com"
   client_id_list = ["sts.amazonaws.com"]
 }
+
+# create a bucket once that will be used as a tfstate storage solution
+resource "aws_s3_bucket" "terraform_state_storage" {
+  bucket_prefix = "terraform-state-storage-"
+}
+
+# block public access
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.terraform_state_storage.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# explicitly set default SSE-S3 encryption for documentation purposes
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.terraform_state_storage.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# enable versioning for the bucket 
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.terraform_state_storage.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# lifecycle policy to keep only the last 3 versions after 1 day to save on storage costs
+resource "aws_s3_bucket_lifecycle_configuration" "this" {
+  bucket = aws_s3_bucket.terraform_state_storage.id
+
+  rule {
+    id     = "keep-last-3-versions-after-1-day"
+    status = "Enabled"
+
+    noncurrent_version_expiration {
+      noncurrent_days           = 1
+      newer_noncurrent_versions = 2
+    }
+  }
+}
